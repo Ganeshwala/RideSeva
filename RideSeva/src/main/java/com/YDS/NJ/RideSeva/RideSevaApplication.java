cd59651870f8609f81447c4e72bf08df932d3a14
ganeshwala <ganeshwalachintan@gmail.com>
2024-06-28 09:31:21 -0700

    Implementing Ride Seva for Jersey City Bhagato

---
+++ RideSeva/src/main/java/com/YDS/NJ/RideSeva/RideSevaApplication.java
@@ -0,0 +1,13 @@
+package com.YDS.NJ.RideSeva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSevaApplication.class, args);
+	}
+
+}
---
+++ RideSeva/src/test/java/com/YDS/NJ/RideSeva/RideSevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YDS.NJ.RideSeva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
